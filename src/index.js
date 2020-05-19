8c19c682c494e8de7ae444e3ff09163c33908f6c
Joshua <joshua@psychotechnology.com>
2020-05-19 23:40:43 +0100

    Project setup with mocha, chai, babel

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+const sayHello = name => {
+    console.log(`Hi ${name}`);
+}
---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('subliminal-config', function() {
+    it('Should have a list of all the messages', function() {
+        
+    })
+
+    it('Should store the correct duration and interval', function() {
+        
+    })
+
+    it('Should allow a random message in the list to be selected', function() {
+
+    })
+
+    it('Should Preserve the order of the message that was passed', function() {
+
+    })
+
+
+})
